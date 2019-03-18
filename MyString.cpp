02dafcae3cf44078139612c66a9d5068dcead2ef
matt180 <33262622+matt180@users.noreply.github.com>
2019-03-18 15:23:42 +0100

    Add move semantics.

--- MyString.cpp
+++ MyString.cpp
@@ -8,14 +8,37 @@ MyString::MyString(const char* str) {
     }
 }
 
+MyString::MyString(MyString&& other)
+    : len_(other.len_)
+    , str_(other.str_)
+    , refcount_(other.refcount_) {
+    other.len_ = 0;
+    other.str_ = nullptr;
+    other.refcount_ = nullptr;
+}
+
 MyString& MyString::operator=(const MyString& other) {
     if (this != &other) {
         destruct();
         len_ = other.len_;
         str_ = other.str_;
+        refcount_ = other.refcount_;
         if (str_) {
             ++(*refcount_);
         }
+    }
+    return *this;
+}
+
+MyString& MyString::operator=(MyString&& other) {
+    if (this != &other) {
+        destruct();
+        len_ = other.len_;
+        other.len_ = 0;
+        str_ = other.str_;
+        other.str_ = nullptr;
+        refcount_ = other.refcount_;
+        other.refcount_ = nullptr;
     }
     return *this;
 }
@@ -32,4 +55,15 @@ void MyString::destruct() {
 
 MyString::~MyString() {
     destruct();
+}
+
+
+
+MyString& MyString::operator+=(const MyString& other) {
+    //if (other != "")
+
+}
+
+MyString& operator+(const MyString& other) {
+
 }
--- MyString.h
+++ MyString.h
@@ -4,26 +4,32 @@
 #include <cstring>
 
 class MyString {
+    private:
+        void destruct();
+
     public:
         explicit MyString(const char* str = "");
 
         MyString(const MyString& other)
-            : str_(other.str_)
+            : len_(other.len_)
+            , str_(other.str_)
             , refcount_(other.refcount_) {
                 if (str_) {
                     ++(*refcount_);
                 }
         }
 
+        MyString(MyString&& other);
+
         size_t size() const {
             return len_;
         }
 
-    private:
-        void destruct();
-
-    public:
         MyString& operator=(const MyString& other);
+
+        MyString& operator=(MyString&& other);
+
+        MyString& operator+=(const MyString& other);
 
         ~MyString();
 
@@ -34,5 +40,7 @@ class MyString {
         char* str_{nullptr};
         int* refcount_{nullptr};
 };
+
+MyString& operator+(const MyString& rhs);
 
 #endif // MYSTRING_H_INCLUDED
